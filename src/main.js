fed47066411cbbaabff7767cad68b1286be1662c
Aleksandr Davydenko <ozziaqueen@gmail.com>
2024-11-25 22:33:13 +0100

    final relise

--- src/main.js
+++ src/main.js
@@ -2,17 +2,17 @@
 import fetchImages from './js/pixabay-api.js';
 import { message, checkData, galleryShow } from './js/render-functions.js';
 
-const btnSearch = document.querySelector('.btn-search'),
+const form = document.querySelector('.search-image-form'),
   searchInputField = document.querySelector('.search-img'),
   gallerySite = document.querySelector('.gallery'),
   loaderImages = document.querySelector('.loader');
 
-btnSearch.addEventListener('click', checkInput);
+form.addEventListener('submit', checkInput);
 
 function checkInput(e) {
   e.preventDefault();
   const searchText = searchInputField.value.trim(); // Поисковый запрос пользователя
-  searchInputField.value = ''; // Очистка поля ввода поискового запроса
+  form.reset(); // Очистка поля ввода поискового запроса
   gallerySite.innerHTML = ''; // Удаление текущей галлереи изображений с сайта
   loaderImages.style.display = 'block'; // Отоброзить прогресс загрузки результатов поиска
   if (!searchText.length) {
